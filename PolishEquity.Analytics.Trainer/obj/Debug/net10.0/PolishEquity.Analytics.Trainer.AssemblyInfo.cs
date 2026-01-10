b8d75ee67a52369a534090c308698639c9e9d5f6
Jakub Dadello <kejdi16try@gmail.com>
2026-01-10 17:07:58 +0100

    update Trainer

--- PolishEquity.Analytics.Trainer/obj/Debug/net10.0/PolishEquity.Analytics.Trainer.AssemblyInfo.cs
+++ PolishEquity.Analytics.Trainer/obj/Debug/net10.0/PolishEquity.Analytics.Trainer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PolishEquity.Analytics.Trainer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3498037ab5421fb944f75cddf167ad90d04c5614")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d41f0bf4ed480cb59e1b2ccdcab730867054eb32")]
 [assembly: System.Reflection.AssemblyProductAttribute("PolishEquity.Analytics.Trainer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PolishEquity.Analytics.Trainer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
