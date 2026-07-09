90b5e95e9472ad1c7077be382baf42a39a6816f8
Jakub Dadello <kejdi16try@gmail.com>
2026-07-09 13:04:25 +0200

    update AzureInferenceApi/Program.cs

--- AzureInferenceApi/Program.cs
+++ AzureInferenceApi/Program.cs
@@ -1,23 +1,32 @@
-using Microsoft.Extensions.ML;
-using Schemas;
+using Microsoft.Extensions.ML; //provide access to the PredictionEnginePool
+using Schemas; //provide access to the input and output schemas  
 
+//create a configuration object for the web application
 var builder = WebApplication.CreateBuilder(args);
 
+//register the PredictionEnginePool (a pool includes a few predictors)
 builder.Services.AddPredictionEnginePool<ModelInput, ModelOutput>()
-    .FromFile("models/LightGBM_model.zip");
+    .FromFile("models/LightGBM_model.zip"); //load the Light GBM model 
 
+//build the web application
 var app = builder.Build();
 
+//define the endpoint
 app.MapPost("/predict", (PredictionEnginePool<ModelInput, ModelOutput> predictionEnginePool, ModelInput input) =>
 {
+
+    //validation
     if (input == null)
     {
         return Results.BadRequest("Input data cannot be null.");
     }
 
+    //prediction
     ModelOutput prediction = predictionEnginePool.Predict(input);
 
+    //return a result
     return Results.Ok(prediction);
 });
 
+//run the web application
 app.Run();
