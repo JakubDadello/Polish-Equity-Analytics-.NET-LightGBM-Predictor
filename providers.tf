2e437acb71c9dc621c43e37956c02575346e06ad
Jakub Dadello <kejdi16try@gmail.com>
2026-07-10 13:24:36 +0200

    update providers.tf

--- providers.tf
+++ providers.tf
@@ -11,4 +11,9 @@ terraform {
 # Microsoft Azure Provider configuration
 provider "azurerm" {
   features {}
+  use_cli = true
+
+  # In version 4.0 of the azurerm provider it became mandatory to explicitly 
+  # set the subscription_id in the provider block
+  subscription_id = var.subscription_id
 }
